c15dc12d06019840c546e6eac62b6d1a4acd22e8
Pintér Lajos <lajos.pinter@isrv.hu>
2017-02-01 17:28:10 +0100

    Add built-in states

--- backend/src/main/java/org/str/backend/GameOfLifeService.java
+++ backend/src/main/java/org/str/backend/GameOfLifeService.java
@@ -3,6 +3,7 @@ package org.str.backend;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -15,4 +16,9 @@ public interface GameOfLifeService {
     @Consumes({ MediaType.APPLICATION_JSON })
     @Path("/nextstate")
     public GameOfLifeState calculateNextState(@QueryParam("state") GameOfLifeState state);
+
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Path("/builtin/{id}")
+    public GameOfLifeState getBuiltInState(@PathParam("id") String identifier);
 }
